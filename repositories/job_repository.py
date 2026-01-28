d079da61ee59b4bde78e68f82924022931bcaaa5
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-28 00:23:02 -0500

    Implement save_all repository method

--- repositories/job_repository.py
+++ repositories/job_repository.py
@@ -12,3 +12,8 @@ class JobRepository:
         with open(self.FILE, "r") as f:
             data = json.load(f)
             return [JobApplication.from_dict(x) for x in data]
+
+    def save_all(self, jobs):
+        os.makedirs("data", exist_ok=True)
+        with open(self.FILE, "w") as f:
+            json.dump([j.to_dict() for j in jobs], f, indent=4)

16fdb21b7aabd43f9de35c2c155ac78934a5d51d
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-28 00:19:03 -0500

    Add to_dict serialization method

--- models/job_application.py
+++ models/job_application.py
@@ -6,3 +6,13 @@ class JobApplication:
         self.status = status
         self.applied_date = applied_date
         self.interview_date = interview_date
+
+    def to_dict(self):
+        return {
+            "id": self.job_id,
+            "company": self.company,
+            "title": self.title,
+            "status": self.status,
+            "applied_date": self.applied_date,
+            "interview_date": self.interview_date
+        }

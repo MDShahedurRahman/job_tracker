26bc480690699a4074de3ac1fc15ae5c963abc3d
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-28 00:26:36 -0500

    Implement add_job business logic

--- services/job_service.py
+++ services/job_service.py
@@ -7,3 +7,9 @@ class JobService:
     def __init__(self):
         self.repo = JobRepository()
         self.jobs = self.repo.load_all()
+
+    def add_job(self, company, title, status):
+        job_id = len(self.jobs) + 1
+        job = JobApplication(job_id, company, title, status, today())
+        self.jobs.append(job)
+        self.repo.save_all(self.jobs)

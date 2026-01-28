3a5cb5a54e80d7e032f22efcdb496a64ed80dc11
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-28 00:18:21 -0500

    Implement JobApplication constructor

--- models/job_application.py
+++ models/job_application.py
@@ -1,1 +1,8 @@
 class JobApplication:
+    def __init__(self, job_id, company, title, status, applied_date, interview_date=None):
+        self.job_id = job_id
+        self.company = company
+        self.title = title
+        self.status = status
+        self.applied_date = applied_date
+        self.interview_date = interview_date

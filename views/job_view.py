086b3635bb15a8bccea9d7dab6fd53dba5ee6fbd
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-29 23:14:16 -0500

    add menu

--- views/job_view.py
+++ views/job_view.py
@@ -1,1 +1,12 @@
 class JobView:
+    def menu(self):
+        print("\n1. Add Job")
+        print("2. View All Jobs")
+        print("3. Update Status")
+        print("4. Delete Job")
+        print("5. Filter by Company")
+        print("6. Filter by Status")
+        print("7. Upcoming Interviews")
+        print("8. Analytics Summary")
+        print("0. Exit")
+        return input("Choose: ")

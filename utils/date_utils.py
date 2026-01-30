12c595b6381011a3d673e76f90123aaddc79f38f
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-29 23:20:00 -0500

    Implement days_until() method

--- utils/date_utils.py
+++ utils/date_utils.py
@@ -3,3 +3,8 @@ from datetime import datetime
 
 def today():
     return datetime.now().strftime("%Y-%m-%d")
+
+
+def days_until(date_str):
+    d = datetime.strptime(date_str, "%Y-%m-%d")
+    return (d - datetime.now()).days
